d1b39dd698f26e98bb5499c599e38fd4a3717270
taka- <takayuki.for.public@gmail.com>
2014-01-25 15:34:35 +0900

    create td from metainfo

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,16 +1,18 @@
 'use strict';
 
+// これはどこにおけば良いのやら。。
+var metainfo = {
+  records: [
+    {property: "id"},
+    {property: "name"},
+  ]
+};
+
 angular.module('magicTableApp')
   .controller('MainCtrl', ['$scope', function ($scope) {
   $scope.positions = [{ Name: "Quarterback", Code: "QB" },
                                { Name: "Wide Receiver", Code: "WR" }
                               ]; 
-    $scope.metainfo = {
-      records: [
-        {property: "id"},
-        {property: "name"},
-      ]
-    };
     $scope.records = [
       {id: 1, name: "andy"},
       {id: 2, name: "brian"},
@@ -18,8 +20,17 @@ angular.module('magicTableApp')
     ];
   }])
 .directive("record", function(){
+     var buildRow = function() {
+       var tds = new Array();
+       for (var i = 0; i < metainfo.records.length; i++) {
+         var m = metainfo.records[i];
+         tds[i] = '<td>{{record["' + m.property + '"]}}</td>';
+       }
+       var v =  '<tr>' + tds.join('') + '</tr>';
+       return v;
+     };
     return {
-        template: "<tr><td>{{record.id}}</td><td>{{record.name}}</td></tr>",
+        template: (function(){return buildRow()})(),
         replace: false,
         scope: {
           record: '='
